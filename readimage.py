b3ca5207ea24a3c698836db53330d4c2255067b1
lappair <athallah1123@gmail.com>
2026-09-08 14:36:08 +0700

    yo

--- readimage.py
+++ readimage.py
@@ -3,6 +3,8 @@ import numpy as np
 
 img = cv.imread('honda.png', cv.IMREAD_GRAYSCALE)
 
+if img is None:
+    raise FileNotFoundError("errorgng")
 cv.imshow("gambar", img)
 cv.waitKey(0)
 cv.destroyAllWindows()
